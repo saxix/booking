a4dade6c2af38ad353168f20a260c18d24a98a2b
Stefano Apostolico <s.apostolico@gmail.com>
2025-01-01 17:47:17 +0100

    updates compose file

--- src/booking/management/commands/demo.py
+++ src/booking/management/commands/demo.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from typing import Any
 
 from django.core.management import BaseCommand
@@ -15,6 +16,13 @@ class Command(BaseCommand):
     def handle(self, *args: Any, **options: Any) -> None:
         from booking.utils.fixtures import Car, CarFactory, Service, ServiceFactory, User
 
+        if "ADMIN_USER" in os.environ:
+            User.objects.create_superuser(
+                os.environ["ADMIN_USER"],
+                email=os.environ["ADMIN_USER"],
+                password=os.environ["ADMIN_PASSWORD"],
+            )
+            self.stdout.write(self.style.SUCCESS("Superuser Successfully created"))
         Car.objects.all().delete()
         CarFactory.create_batch(len(MODELS))
         ServiceFactory.create_batch(10)
